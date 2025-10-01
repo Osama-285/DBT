ae8972d8d5d5006d6b14bed92966866ccc18d4b0
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2025-10-01 19:38:04 +0500

    practice macro

---
+++ databricks_practice/practice_project_1/macros/classify_year.sql
@@ -0,0 +1,8 @@
+{% macro classify_year(total_sales) %}
+    case
+        when {{ total_sales }} < 50000 then 'Low Value Year'
+        when {{ total_sales }} >= 50000 and {{ total_sales }} < 80000 then 'MID value Year'
+        when {{ total_sales }} >= 90000 then 'High value Year'
+        else 'Other'
+    end
+{% endmacro %}
